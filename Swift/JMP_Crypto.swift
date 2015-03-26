d595f1c2bc866b39f831e3f2b13a7d4ad966f5ba
Satoru Ogura <satoru.ogura@me.com>
2015-03-26 17:58:47 +0900

    Add CCCrypto

--- Swift/JMP_Crypto.swift
+++ Swift/JMP_Crypto.swift
@@ -11,3 +11,75 @@ SHA1( p: NSData ) -> NSData {
 	return NSData( bytes: w, length: Int( CC_SHA1_DIGEST_LENGTH ) )
 }
 
+func
+DataCryptedByAES( operation: CCOperation, p: NSData, key: NSData, _ options: CCOptions = CCOptions( kCCOptionPKCS7Padding ), _ iv: NSData? = nil ) -> ( CCCryptorStatus, NSData ) {
+	var	wKeyLength = 0
+	if		key.length < kCCKeySizeAES128	{ assert( false ) }
+	else if	key.length < kCCKeySizeAES192	{ wKeyLength = kCCKeySizeAES128 }
+	else if	key.length < kCCKeySizeAES256	{ wKeyLength = kCCKeySizeAES192 }
+	else									{ wKeyLength = kCCKeySizeAES256 }
+
+	if iv != nil { assert( iv!.length == kCCBlockSizeAES128 ) }
+
+	var	wLength = UInt( ( ( p.length + kCCBlockSizeAES128 - 1 ) / kCCBlockSizeAES128 ) * kCCBlockSizeAES128 )
+	let	v = NSMutableData( length: Int( wLength ) )!
+	let	s: CCCryptorStatus = CCCrypt(
+		operation
+	,	CCAlgorithm( kCCAlgorithmAES )
+	,	options
+	,	key.bytes
+	,	UInt( wKeyLength )
+	,	iv != nil ? iv!.bytes : nil
+	,	p.bytes
+	,	UInt( p.length )
+	,	v.mutableBytes
+	,	wLength
+	,	&wLength
+	)
+	v.length = Int( wLength )
+	return ( s, v )
+}
+
+func
+DataEncryptedByAES( p: NSData, key: NSData, _ options: CCOptions = CCOptions( kCCOptionPKCS7Padding ), _ iv: NSData? = nil ) -> ( CCCryptorStatus, NSData ) {
+	return DataCryptedByAES( CCOperation( kCCEncrypt ), p, key, options, iv )
+}
+
+func
+DataDecryptedByAES( p: NSData, key: NSData, _ options: CCOptions = CCOptions( kCCOptionPKCS7Padding ), _ iv: NSData? = nil ) -> ( CCCryptorStatus, NSData ) {
+	return DataCryptedByAES( CCOperation( kCCDecrypt ), p, key, options, iv )
+}
+
+func
+DataCryptedByBlowfish( operation: CCOperation, p: NSData, key: NSData, _ options: CCOptions = CCOptions( kCCOptionPKCS7Padding ), _ iv: NSData? = nil ) -> ( CCCryptorStatus, NSData ) {
+	if iv != nil { assert( iv!.length == kCCBlockSizeBlowfish ) }
+
+	var	wLength = UInt( ( ( p.length + kCCBlockSizeBlowfish - 1 ) / kCCBlockSizeBlowfish ) * kCCBlockSizeBlowfish )
+	let	v = NSMutableData( length: Int( wLength ) )!
+	let	s: CCCryptorStatus = CCCrypt(
+		operation
+	,	CCAlgorithm( kCCAlgorithmBlowfish )
+	,	options
+	,	key.bytes
+	,	UInt( key.length )
+	,	iv != nil ? iv!.bytes : nil
+	,	p.bytes
+	,	UInt( p.length )
+	,	v.mutableBytes
+	,	wLength
+	,	&wLength
+	)
+	v.length = Int( wLength )
+	return ( s, v )
+}
+
+func
+DataEncryptedByBlowfish( p: NSData, key: NSData, _ options: CCOptions = CCOptions( kCCOptionPKCS7Padding ), _ iv: NSData? = nil ) -> ( CCCryptorStatus, NSData ) {
+	return DataCryptedByBlowfish( CCOperation( kCCEncrypt ), p, key, options, iv )
+}
+
+func
+DataDecryptedByBlowfish( p: NSData, key: NSData, _ options: CCOptions = CCOptions( kCCOptionPKCS7Padding ), _ iv: NSData? = nil ) -> ( CCCryptorStatus, NSData ) {
+	return DataCryptedByBlowfish( CCOperation( kCCDecrypt ), p, key, options, iv )
+}
+
